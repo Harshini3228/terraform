5eaefcf208b2ce1ddf540fb8b2d55394ac0444ec
Harshini3228 <harshini3228@gmail.com>
2023-11-29 17:08:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,7 +5,9 @@ terraform {
       version = "=3.0.0"
     }
   }
-
+variable "client_secret" {
+ 3zf8Q~R9q4uPA9t2p.u2bxUNr5ygVC5oVcpnacoo
+}
   backend "azurerm" {
     resource_group_name  = "Harshini-Training"
     storage_account_name = "harshinistorageaccount"
@@ -18,4 +20,8 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  client_id       = "9f853451-c0f9-4f88-be1d-2e578bbb76be"
+  client_secret   = var.client_secret
+  tenant_id       = "4284bc2c-7bea-4e03-b48a-128cd2886ddb"
+  subscription_id = "fb1c9914-31d0-4fa1-a772-739012419bda"
 }
