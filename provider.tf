86189002243b97154019cd860a5e7ca60fd0c23a
Harshini3228 <harshini3228@gmail.com>
2023-11-30 09:13:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -17,8 +17,4 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  client_id       = "9f853451-c0f9-4f88-be1d-2e578bbb76be"
-  client_secret   = "3zf8Q~R9q4uPA9t2p.u2bxUNr5ygVC5oVcpnacoo"
-  tenant_id       = "4284bc2c-7bea-4e03-b48a-128cd2886ddb"
-  subscription_id = "fb1c9914-31d0-4fa1-a772-739012419bda"
 }
